2be03ca10089815a9ed7610edbd9b1c87e1cd915
Divya Srivastava <divyasrivastava1113@gmail.com>
2024-08-06 15:27:11 -0700

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>About Me</h2>
+            <p>This is a brief description about yourself.</p>
+        </section>
+    );
+}
+
+export default About
